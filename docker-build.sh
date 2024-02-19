328c33308c3590cd5afd6aac2512e7adea076739
Arjan van Olphen <arjan@scanframe.nl>
2024-02-19 17:33:42 +0100

    Added pull command for pulling latest image and added help text for docker-build.sh script.

--- docker-build.sh
+++ docker-build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+# GEt the script directory.
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 # Build directory used for Docker to prevent mixing.
 BUILD_DIR="${SCRIPT_DIR}/cmake-build/docker"
+# Set the image name to be used.
+IMG_NAME="nexus.scanframe.com:8090/gnu-cpp:dev"
+# Hostname for the docker container.
+HOSTNAME="cpp-builder"
 # Create the docker binary build directory.
 mkdir -p "${SCRIPT_DIR}/cmake-build/docker"
 # Function which runs the docker build.sh script.
 function docker_run {
-	local IMG_NAME HOSTNAME
-	# Set the image name to be used.
-	IMG_NAME="nexus.scanframe.com:8090/gnu-cpp:dev"
-	# Hostname for the docker container.
-	HOSTNAME="cpp-builder"
 	docker run \
 		--rm \
 		--interactive \
@@ -29,9 +28,18 @@ function docker_run {
 }
 
 if [[ $# -eq 0 ]]; then
+	echo "Same as 'build.sh' script but running from Docker image '${IMG_NAME}'.
+
+To pull the latest image for use the 'pull' command explicitly for this script.
+Now entering container using bash to allow inspection.
+"
 	# Execute the build script from the Docker image.
 	docker_run bash
 else
+	# When only pull has been passed pull the latest image.
+	if [[ $# -eq 1  && "${1}" == "pull" ]]; then
+		docker pull "${IMG_NAME}"
+	fi
 	# Execute the build script from the Docker image.
 	docker_run /mnt/project/build.sh "${@}"
 fi
