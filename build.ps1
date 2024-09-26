2c9d4ae825a8d3bcef9a8dd956fd8ec779287826
Arjan van Olphen <arjan@scanframe.nl>
2024-09-26 15:03:33 +0200

    Replaced project-root bash scripts with symlinks to cmake-lib git-module.

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 # Gets the current script directory and make it the current working.
 Push-Location -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
 # Run the imported script.
-. ".\cmake\lib\bin\Build.ps1"
+. ".\cmake\lib\bin\build.ps1"
 ProcessBuild $MyInvocation $MyInvocation.Line
 # Restore the previous working directory location.
 Pop-Location
--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-
-script_dir="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)"
-SCRIPT_DIR="${script_dir}" "${script_dir}/cmake/lib/bin/Build.sh" "${@}"
+cmake/lib/bin/build.sh
