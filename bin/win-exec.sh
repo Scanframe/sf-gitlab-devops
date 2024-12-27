adc567670b5564397cba21fbfb09ff1802cb52f4
Arjan van Olphen <arjan@scanframe.nl>
2024-12-27 17:24:06 +0100

    Fixed the Docker image path for GitLab CI/CD pipeline configuration. Updated shell scripts called by tests.

--- bin/win-exec.sh
+++ bin/win-exec.sh
@@ -3,4 +3,4 @@
 # Get the current script directory.
 dir="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Set the env variables for the script to act on.
-EXECUTABLE_DIR="${dir}/win64" "${dir}/../cmake/lib/bin/WineExec.sh" "${@}"
+EXECUTABLE_DIR="${dir}/win64${SF_OUTPUT_DIR_SUFFIX}" "${dir}/../cmake/lib/bin/WineExec.sh" "${@}"
