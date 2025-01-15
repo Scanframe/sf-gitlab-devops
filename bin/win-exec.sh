8a8a628030081c22d090f1eb1b8327d941749d6d
Arjan van Olphen <arjan@scanframe.nl>
2025-01-15 16:33:26 +0100

    Changes to cmake-lib.

--- bin/win-exec.sh
+++ bin/win-exec.sh
@@ -2,5 +2,12 @@
 
 # Get the current script directory.
 dir="$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)"
-# Set the env variables for the script to act on.
-EXECUTABLE_DIR="${dir}/win64${SF_OUTPUT_DIR_SUFFIX}" "${dir}/../cmake/lib/bin/WineExec.sh" "${@}"
+
+# When running from Cygwin Windows is the host so Wine is not there.
+if [[ "$(uname -o)" != "Cygwin" ]]; then
+	# Set the env variables for the script to act on.
+	EXECUTABLE_DIR="${dir}/win64${SF_OUTPUT_DIR_SUFFIX}" "${dir}/../cmake/lib/bin/WineExec.sh" "${@}"
+else
+	# Set the env variables for the script to act on.
+	EXECUTABLE_DIR="${dir}/win64${SF_OUTPUT_DIR_SUFFIX}" "${dir}/../cmake/lib/bin/WindowsExec.sh" "${@}"
+fi
